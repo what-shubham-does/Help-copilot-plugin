2dffd4e794de94a98fa986495e34c4539ac1fa26
what-shubham-does <route2shubham@gmail.com>
2026-05-22 21:49:57 +0530

    Update header-toggle.php

--- templates/header-toggle.php
+++ templates/header-toggle.php
@@ -1,5 +1,4 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
-
 <div id="hcHeaderToggle" class="hc-toggle-pill">
     <button class="hc-toggle-opt active" data-mode="browse">Browse</button>
     <button class="hc-toggle-opt"        data-mode="ask">Ask</button>
